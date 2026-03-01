750ee363898c6b0393fbb657878e02a57fa00152
ZihongLin <1046298030@qq.com>
2026-02-28 22:47:54 -0800

    Initial Commit

---
+++ DriverID/DriverIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriverIDApp.swift
+//  DriverID
+//
+//  Created by Zihong Lin on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct DriverIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DriverIDTests/DriverIDTests.swift
@@ -0,0 +1,17 @@
+//
+//  DriverIDTests.swift
+//  DriverIDTests
+//
+//  Created by Zihong Lin on 2/28/26.
+//
+
+import Testing
+@testable import DriverID
+
+struct DriverIDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
